0726d21b5f2ffc4fad342aedb25b682773495443
DESKTOP-G001VRU\crlis <crlis@naver.com>
2022-02-08 22:58:39 +0900

    경로 수정

--- src/test/Test.java
+++ src/test/Test.java
@@ -34,7 +34,7 @@ public class Test {
 					
 					String second = ie2.next().text();
 					System.out.println(ie1.next().text()+"\t"+second+"\t"+ie3.next().text());
-					String path = "C:\\Users\\crlis\\OneDrive\\바탕 화면\\이클립스 실습\\폴더만들기\\" +second; // 폴더 경로
+					String path = "파일을 생성할 경로" +second; // 폴더 경로
 					File Folder = new File(path);
 
 					// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
